b8f4e56f6dbdb9e54b4dd25bf802920bbb444fb4
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-05-20 19:12:40 -0300

    funcion para generar cartas random

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -20,19 +20,26 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     
   }
-
+  // funcion para generar numeros aleatorios
   getRandomInt(min: number, max: number): any {
     return Math.floor(Math.random() * (max - min)) + min;
   }
-
+  // funcion para desordenar el array
+  shuffle(array: number[]): number[] {
+    array.sort(() => Math.random() - 0.5);
+    return array;
+  }
+  // funcion para obtener la lista de pokemons
   getPokemonList(): void {
     this.pokemonList = [];
     for (let i = 0; i < this.pokemonLength; i++) {
       this.pokemonList.push(this.getRandomInt(this.min, this.max));
     }
+    this.pokemonList = this.pokemonList.concat(this.pokemonList);
+    this.pokemonList = this.shuffle(this.pokemonList);
   }
-
-  getPokemon(pokemonLength: number): boolean{
+  // funcion para generar las cartas
+  getCards(pokemonLength: number): boolean{
     this.pokemonLength = pokemonLength;
     this.getPokemonList();
     return this.gameStarted = true;
