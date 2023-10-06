93030de218fe149be0c0e1b59483de0c0f859456
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-10-06 08:46:49 -0300

    preparacion para actualizacion de angular

--- src/app/game/start-screen/start-screen.component.ts
+++ src/app/game/start-screen/start-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 import { GameParams } from '../interface';
 import { CardsService } from '../services/cards.service';
@@ -15,14 +15,14 @@ export class StartScreenComponent implements OnInit {
   title: string = "Pokemon Memory";
   @Input() pokemonScreen: number = 0;
   @Output() startGameEvent = new EventEmitter<GameParams>();
-  form: FormGroup;
+  form: UntypedFormGroup;
 
 
 
   constructor(
     public cardsService: CardsService,
     public gameplay: GameplayService,
-    private formBuilder: FormBuilder
+    private formBuilder: UntypedFormBuilder
   ) {
     this.form = this.formBuilder.group({
       player: ['', [Validators.required]],
